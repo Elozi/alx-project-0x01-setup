65599322229dd229259c424bd55387678d6e40ba
Elozi <Abbyjamezzz@gmail.com>
2025-06-07 22:06:11 +0100

    task 2

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,11 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage: React.FC = () => {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
-        <p>List of posts will appear here.</p>
-      </div>
-    );
-  };
-  
-  export default PostsPage;
-  
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+        {/* Add post content here */}
+      </main>
+    </div>
+  );
+};
+
+export default PostsPage;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Users Page</h1>
-        <p>List of users will appear here.</p>
-      </div>
-    );
-  };
-  
-  export default UsersPage;
-  
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold">Users Page</h1>
+        {/* Add user content here */}
+      </main>
+    </div>
+  );
+};
+
+export default UsersPage;
